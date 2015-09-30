9314d3277365a12c1e06d18a218dfef0c95b172a
tonda <tondakal@gmail.com>
2015-09-30 11:21:32 +0200

    libs

--- src/android/OCR.java
+++ src/android/OCR.java
@@ -12,11 +12,34 @@ import android.content.Intent;
 
 public class OCR extends CordovaPlugin {
 
+    private static final int OCR_ACTIVITY=200;
+ protected CallbackContext callbackContext;
+
+public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == OCR_ACTIVITY) {
+            if (resultCode == RESULT_OK) {
+                String returnedResult = data.getData().toString();
+		this.callbackContext.success(returnedResult);
+		return true;
+//                TextView result = (TextView) findViewById(R.id.textView);
+//                result.setText(returnedResult);
+            }
+        }
+	return false;
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
             throws JSONException {
-
+	this.callbackContext = callbackContext;
+	if (action.equals("sayHello")){
+		Context context=this.cordova.getActivity().getApplicationContext();
+		Intent intent = new Intent(context, CaptureActivity.class);
+                context.startActivityForResult(intent,MainActivity.OCR_ACTIVITY);
+	}
+/*
         if (action.equals("sayHello")){
             Context context =  cordova.getActivity().getApplicationContext();
             //Intent intent = new Intent(context,CaptureActivity.class);
@@ -32,6 +55,7 @@ public class OCR extends CordovaPlugin {
         }
 
         return false;
+*/
         
     }
 
